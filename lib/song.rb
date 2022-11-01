c05bd622e6b764c6e815a8df50b9e71ef4c3edea
ruthwekesa <wekesaruth569@gmail.com>
2022-11-01 22:01:39 +0300

    completed assignment

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,7 @@
-require_relative "./interactive_record.rb"
+require_relative './interactive_record'
 
 class Song < InteractiveRecord
-
-  self.column_names.each do |col_name|
+  column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
-
 end
